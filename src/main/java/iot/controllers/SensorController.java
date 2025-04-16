c1e3980d919b5d590555aa7cbf2c93d022d1cde6
Kanao <truongphuoc147@gmail.com>
2025-04-16 11:04:32 +0700

    update

--- src/main/java/iot/controllers/SensorController.java
+++ src/main/java/iot/controllers/SensorController.java
@@ -4,14 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import iot.dto.SensorDataDto;
-import iot.dto.SensorDataDto.CurrentTemperatureResponseDto;
 import iot.model.Humidity;
 import iot.model.Light;
 import iot.model.Temperature;
@@ -26,7 +25,7 @@ public class SensorController {
 	private SensorRecordingService sensorRecordingService;
 
 	// Get current temperature
-	@GetMapping("/temperature/stream")
+	@PostMapping("/temperature/stream")
 	public ResponseEntity<ApiResponse<SensorDataDto.CurrentTemperatureResponseDto>> getCurrentTemperature(
 			@Valid @RequestBody SensorDataDto.CurrentTemperatureRequestDto request) {
 		// Simulate a temperature reading
@@ -51,26 +50,29 @@ public class SensorController {
 				.build());
 	}
 
-	@GetMapping("/temperature/by-date")
-    public ResponseEntity<ApiResponse<List<SensorDataDto.CurrentTemperatureResponseDto>>> getAllTemperaturesByDate(
-		@Valid @RequestParam(name = "date") String date) {
-    
-        List<Temperature> list = sensorRecordingService.getTemperaturesByDate(date);
-    
-        List<SensorDataDto.CurrentTemperatureResponseDto> dtoList = list.stream().map(temp -> SensorDataDto.CurrentTemperatureResponseDto.builder()
-                .value(temp.getValue())
-                .unit(temp.getUnit())
-                .status(temp.getStatus())
-                .date(temp.getCreatedDate()) 
-                .build()).toList();
-    
-        return ResponseEntity.ok(ApiResponse.<List<SensorDataDto.CurrentTemperatureResponseDto>>builder()
-                .message("List of temperatures for: " + date)
-                .data(dtoList)
-                .build());
-    }
+	@PostMapping("/temperature/by-date")
+	public ResponseEntity<ApiResponse<List<SensorDataDto.CurrentTemperatureResponseDto>>> getAllTemperaturesByDate(
+			@Valid @RequestParam(name = "date") String date) {
+
+		List<Temperature> list = sensorRecordingService.getTemperaturesByDate(date);
+
+		List<SensorDataDto.CurrentTemperatureResponseDto> dtoList = list.stream()
+				.map(temp -> SensorDataDto.CurrentTemperatureResponseDto.builder()
+						.value(temp.getValue())
+						.unit(temp.getUnit())
+						.status(temp.getStatus())
+						.date(temp.getCreatedDate())
+						.build())
+				.toList();
+
+		return ResponseEntity.ok(ApiResponse.<List<SensorDataDto.CurrentTemperatureResponseDto>>builder()
+				.message("List of temperatures for: " + date)
+				.data(dtoList)
+				.build());
+	}
+
 	// Get current humidity
-	@GetMapping("/humidity/stream")
+	@PostMapping("/humidity/stream")
 	public ResponseEntity<ApiResponse<SensorDataDto.CurrentHumidityResponseDto>> getCurrentHumidity(
 			@Valid @RequestBody SensorDataDto.CurrentHumidityRequestDto request) {
 		// Simulate a humidity reading
@@ -96,23 +98,27 @@ public class SensorController {
 				.build());
 
 	}
-	@GetMapping("/humidity/by-date")
+
+	@PostMapping("/humidity/by-date")
 	public ResponseEntity<ApiResponse<List<SensorDataDto.CurrentHumidityResponseDto>>> getAllHumidityByDate(
 			@RequestParam(name = "date") String date) {
 		List<Humidity> list = sensorRecordingService.getHumidityByDate(date);
-		List<SensorDataDto.CurrentHumidityResponseDto> dtoList = list.stream().map(h -> SensorDataDto.CurrentHumidityResponseDto.builder()
-				.value(h.getValue())
-				.unit(h.getUnit())
-				.status(h.getStatus())
-				.date(h.getCreatedDate())
-				.build()).toList();
+		List<SensorDataDto.CurrentHumidityResponseDto> dtoList = list.stream()
+				.map(h -> SensorDataDto.CurrentHumidityResponseDto.builder()
+						.value(h.getValue())
+						.unit(h.getUnit())
+						.status(h.getStatus())
+						.date(h.getCreatedDate())
+						.build())
+				.toList();
 		return ResponseEntity.ok(ApiResponse.<List<SensorDataDto.CurrentHumidityResponseDto>>builder()
 				.message("List of humidity values for: " + date)
 				.data(dtoList)
 				.build());
 	}
+
 	// Get current light
-	@GetMapping("/light/stream")
+	@PostMapping("/light/stream")
 	public ResponseEntity<ApiResponse<SensorDataDto.CurrentLightResponseDto>> getCurrentHumidity(
 			@Valid @RequestBody SensorDataDto.CurrentLightRequestDto request) {
 		// Simulate a humidity reading
@@ -138,16 +144,18 @@ public class SensorController {
 				.build());
 	}
 
-		@GetMapping("/light/by-date")
+	@PostMapping("/light/by-date")
 	public ResponseEntity<ApiResponse<List<SensorDataDto.CurrentLightResponseDto>>> getAllLightByDate(
 			@RequestParam(name = "date") String date) {
 		List<Light> list = sensorRecordingService.getLightByDate(date);
-		List<SensorDataDto.CurrentLightResponseDto> dtoList = list.stream().map(l -> SensorDataDto.CurrentLightResponseDto.builder()
-				.value(l.getValue())
-				.unit(l.getUnit())
-				.status(l.getStatus())
-				.date(l.getCreatedDate())
-				.build()).toList();
+		List<SensorDataDto.CurrentLightResponseDto> dtoList = list.stream()
+				.map(l -> SensorDataDto.CurrentLightResponseDto.builder()
+						.value(l.getValue())
+						.unit(l.getUnit())
+						.status(l.getStatus())
+						.date(l.getCreatedDate())
+						.build())
+				.toList();
 		return ResponseEntity.ok(ApiResponse.<List<SensorDataDto.CurrentLightResponseDto>>builder()
 				.message("List of light values for: " + date)
 				.data(dtoList)
